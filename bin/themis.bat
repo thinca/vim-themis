e1aeee10fb52f95a08a7c967436e0ae5bee038f1
thinca <thinca@gmail.com>
2016-03-20 21:09:58 +0900

    Do not create swapfile

--- bin/themis.bat
+++ bin/themis.bat
@@ -16,7 +16,7 @@ if not exist "%STARTUP_SCRIPT%" (
 )
 
 rem FIXME: Some wrong case exists in passing the argument list.
-%THEMIS_VIM% -u NONE -i NONE -N -e -s --cmd "source %STARTUP_SCRIPT%" -- %* 2>&1
+%THEMIS_VIM% -u NONE -i NONE -n -N -e -s --cmd "source %STARTUP_SCRIPT%" -- %* 2>&1
 exit /b %ERRORLEVEL%
 
 :get_realpath
